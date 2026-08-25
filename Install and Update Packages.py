422b8c858dc0a4ebbca71d16bac4138e35879426
Brian Blais <bblais@gmail.com>
2026-08-25 11:42:21 -0400

    first

---
+++ Install and Update Packages.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Sprint #1
+
+# In[ ]:
+
+
+get_ipython().system('pip install "git+https://github.com/bblais/Game" --upgrade')
+
+
+# In[ ]:
+
+
+import Game
+
+
+# If you happen the error "Cannot find command 'git'", then you need to install git.
+# 
+# - open an anaconda prompt
+# - type `conda install -c anaconda git`
+
+# if you get this error:
+
+# In[ ]:
+
+
+from game import *
+
+
+# Remember that `Game` is different than `game`.
+
+# In[ ]:
+
+
+
+
+
+# ## Sprint #2
+
+# Run this on the robot to update the software
+
+# In[ ]:
+
+
+pip install "git+https://github.com/bblais/Robot373" --upgrade
+
+
+# ## Sprint #3
+
+# - Windows comes with ssh
+# - install Cyberduck for file transfer: https://cyberduck.io/
+
+# ## Sprint #4
+
+# In[ ]:
+
+
+get_ipython().system('pip install scikit-learn')
+
+
+# In[ ]:
+
+
+get_ipython().system('pip install "git+https://github.com/bblais/classy" --upgrade')
+
+
+# In[ ]:
+
+
+get_ipython().system('pip install "git+https://github.com/bblais/NumpyNet" --upgrade')
+
+
+# In[ ]:
+
+
+get_ipython().system('pip install tqdm')
+
+
+# this one is for cv2
+
+# In[ ]:
+
+
+get_ipython().system('pip install opencv-python')
+
+
+# In[ ]:
+
+
+
+
